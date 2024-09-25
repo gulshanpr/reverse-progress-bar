79a726770be575c349c0a56b2dc56ea5257838d7
gulshanpr <gulshankumarprasad04@gmail.com>
2024-09-26 00:21:05 +0530

    final image generation for now

--- src/lib/generateImg.ts
+++ src/lib/generateImg.ts
@@ -1,48 +1,55 @@
-const { createCanvas } = require('canvas');
-const fs = require('fs');
-const path = require('path');
+const { createCanvas } = require("canvas");
+const fs = require("fs");
+const path = require("path");
 
 function generateProgressBarImage(day: any) {
   const width = 1740;
   const height = 346;
   const parts = 365;
+  const padding = 55;
 
   const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = '#ffffff';
+  ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, width, height);
 
-  const imageBorderWidth = 10;
-  ctx.strokeStyle = '#000000';
+  const imageBorderWidth = 6;
+  ctx.strokeStyle = "#000000";
   ctx.lineWidth = imageBorderWidth;
-  ctx.strokeRect(imageBorderWidth / 2, imageBorderWidth / 2, width - imageBorderWidth, height - imageBorderWidth);
+  ctx.strokeRect(padding, padding, width - padding * 2, height - padding * 2);
 
   const barHeight = 170;
-  const barX = 50;
-  const barY = 88;
-  const barWidth = width - 100;
+  const barX = padding + 45;
+  const barY = padding + 35;
+  const barWidth = width - (padding + 50) * 2;
   const partWidth = barWidth / parts;
 
-  const progressBarBorderWidth = 5;
-  ctx.strokeStyle = '#000000';
+  const progressBarBorderWidth = 6;
+  ctx.strokeStyle = "#000000";
   ctx.lineWidth = progressBarBorderWidth;
   ctx.strokeRect(barX, barY, barWidth, barHeight);
 
-  ctx.fillStyle = '#000000';
+  ctx.fillStyle = "#3D8240";
   ctx.fillRect(barX, barY, partWidth * day, barHeight);
 
-  ctx.fillStyle = '#D4D4D4';
-  ctx.fillRect(barX + partWidth * day, barY, barWidth - partWidth * day, barHeight);
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(
+    barX + partWidth * day,
+    barY,
+    barWidth - partWidth * day,
+    barHeight
+  );
 
-  // ctx.fillStyle = '#000000';
-  // ctx.font = '50px Arial';
-  // ctx.fillText(`Day ${day}/365`, width / 2 - 50, barY + barHeight + 40);
+  // const percentage = (day/365 * 100).toFixed(1);
+  // ctx.fillStyle = "#000000";
+  // ctx.font = "50px Arial";
+  // ctx.fillText(`${percentage}% left of 2024` , width / 2 - 180 , barY + barHeight - 217);
 
-  const buffer = canvas.toBuffer('image/png');
+  const buffer = canvas.toBuffer("image/png");
   fs.writeFileSync(path.join(__dirname, `progress-bar-day-${day}.png`), buffer);
 
   return buffer;
 }
 
-generateProgressBarImage(268);
+generateProgressBarImage(365 - 268);
